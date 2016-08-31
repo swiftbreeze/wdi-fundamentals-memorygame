8a37948f532ffef3e0dd837b78241ce62d3abe28
carolinewilson <caroline@bizzby.com>
2016-08-31 16:09:01 +0100

    create alert logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,8 +1,14 @@
-console.log("JS file is connected to HTML! Woo!")
-
 //create variables
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+
+if (cardTwo === cardFour) {
+	alert("You found a match!")
+} else if (cardThree === cardFour) {
+	alert("You found a match!")
+} else {
+	alert("Sorry, try again")
+} 
