176b652d83371f8b824bac5df99b08d0fe6f217a
carolinewilson <caroline@bizzby.com>
2016-09-01 18:05:48 +0100

    added logic to create cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,10 +5,33 @@ var cardThree = "king";
 var cardFour = "king";
 
 
-if (cardTwo === cardFour) {
-	alert("You found a match!")
-} else if (cardThree === cardFour) {
-	alert("You found a match!")
-} else {
-	alert("Sorry, try again")
-} 
+// if (cardTwo === cardFour) {
+// 	alert("You found a match!")
+// } else if (cardThree === cardFour) {
+// 	alert("You found a match!")
+// } else {
+// 	alert("Sorry, try again")
+// } 
+
+
+// create function to build board
+function createBoard() {
+for (var i = 0; i < 4; i++) {
+  // create card
+  var newCard = document.createElement('div');
+
+  // add a class... 
+  newCard.className = 'card';
+
+  // append card to board
+  document.getElementById('game-board').appendChild(newCard);
+}
+}
+
+// event to create board
+
+// find button element and store in variable
+var button = document.getElementById('create-btn');
+
+// set up event handler
+button.addEventListener('click', createBoard);
