58b0dfa5f713e24dd09d088440b60de977dc3a40
carolinewilson <caroline@bizzby.com>
2016-09-06 22:31:55 +0100

    create stats and styling updates

--- starter-code/main.js
+++ starter-code/main.js
@@ -11,13 +11,14 @@ function createBoard() {
 		board.appendChild(newCard);
 	}
 }
+createBoard();
 
 function isTwoCards() {
 	cardsInPlay.push(this.getAttribute('data-card'));
 	if (this.getAttribute('data-card') === 'queen') {
-		this.innerHTML = '<img src="queen-clubs.png" alt="Queen of Clubs" />';  
+		this.innerHTML = '<img src="https://pixabay.com/static/uploads/photo/2012/04/11/13/57/clubs-28334_960_720.png" alt="Queen of Clubs" />';  
 	} else {
-		this.innerHTML = '<img src="king-hearts.png" alt="King of Hearts" />';
+		this.innerHTML = '<img src="https://pixabay.com/static/uploads/photo/2012/04/11/15/10/hearts-28424_960_720.png" alt="King of Hearts" />';
 	}
 	if (cardsInPlay.length === 2) {
 		isMatch(cardsInPlay);
@@ -28,12 +29,37 @@ function isTwoCards() {
 function isMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
+		score = score +1;
+		played = played + 1;
+		updateStats();
 	} else {
 		alert("Sorry, try again");
+		played = played + 1;
+		updateStats();
 	} 
 	for (var i=0; i<cards.length; i++) {
 		document.getElementsByClassName('card')[i].innerHTML="";
 	}
 }
 
-createBoard();
+// stats counters
+var displayedScore = document.getElementById('score');
+var displayedPlayed = document.getElementById('played');
+var displayedSuccess = document.getElementById('success');
+var score= 0;
+var played = 0;
+var resetButton = document.getElementById('reset');
+
+function updateStats() {
+  displayedScore.innerHTML=score; // update displayed score
+  displayedPlayed.innerHTML=played; // update displayed score
+}
+
+function resetStats(){
+	score = 0;
+	played = 0;
+	success = (played / score) * 100;
+	displayedScore.innerHTML=score;
+	displayedPlayed.innerHTML=played;
+}
+resetButton.addEventListener('click',resetStats);
