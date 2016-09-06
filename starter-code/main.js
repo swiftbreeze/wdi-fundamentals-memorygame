04f422dd15b5689a4e3fca9a862f09f8b29b7a46
carolinewilson <caroline@bizzby.com>
2016-09-06 18:49:26 +0100

    remove button to create

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,44 +1,39 @@
-// find button element and store in variable
-var buttonCreate = document.getElementById('btn-create');
-// set up click handler
-buttonCreate.addEventListener('click', createBoard);
-
 var cards = ['queen','queen','king','king'];
 var cardsInPlay = [];
+var board = document.getElementById('game-board');
 
-// builds game board
 function createBoard() {
-  for (var i = 0; i < cards.length; i++) {
-    var newCard = document.createElement('div'); // create card
-    newCard.className = 'card'; // add card class 
-    newCard.setAttribute('data-card', cards[i]); // set data-card
-    newCard.addEventListener('click', isTwoCards); // check cardsInPlay for match on each click
-    document.getElementById('game-board').appendChild(newCard); // append card to board
-  }
+	for (var i = 0; i < cards.length; i++) {
+		var newCard = document.createElement('div');
+		newCard.className = 'card'; 
+		newCard.setAttribute('data-card', cards[i]);
+		newCard.addEventListener('click', isTwoCards);
+		board.appendChild(newCard);
+	}
 }
 
-//checks to see if there are cards in play
 function isTwoCards() {
-  if (this.getAttribute('data-card') === 'queen') {
-   this.innerHTML = '<img src="queen-clubs.png" alt="Queen of Clubs" />';  
-  } else {
-    this.innerHTML = '<img src="king-hearts.png" alt="King of Hearts" />';
- }
-  cardsInPlay.push(this.getAttribute('data-card')); // add card to array of cards in play
-  if (cardsInPlay.length === 2) { // if you have two cards in play check for a match
-
-    isMatch(cardsInPlay); // pass the cardsInPlay as an argument to isMatch function
-    cardsInPlay = []; // clear cards in play array for next try
-  } 
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'queen') {
+		this.innerHTML = '<img src="queen-clubs.png" alt="Queen of Clubs" />';  
+	} else {
+		this.innerHTML = '<img src="king-hearts.png" alt="King of Hearts" />';
+	}
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = []; 
+	} 
 }
 
 function isMatch(){
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-      alert("You found a match!");
-  } else {
-      alert("Sorry, try again");
-  } 
-  for (var i=0; i<cards.length; i++) {
-      document.getElementsByClassName('card')[i].innerHTML="";
-    }
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again");
+	} 
+	for (var i=0; i<cards.length; i++) {
+		document.getElementsByClassName('card')[i].innerHTML="";
+	}
 }
+
+createBoard();
