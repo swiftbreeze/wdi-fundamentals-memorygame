b29ea05ef74e1e5720d8ab2bd1b18cb9c989b0b4
carolinewilson <caroline@bizzby.com>
2016-09-07 14:21:53 +0100

    add timeout to delay alert aand use relative image paths

--- starter-code/main.js
+++ starter-code/main.js
@@ -8,33 +8,36 @@ function createBoard() {
 		newCard.className = 'card'; 
 		newCard.setAttribute('data-card', cards[i]);
 		newCard.addEventListener('click', isTwoCards);
+//  		newCard.addEventListener('click', flipCheck);
 		board.appendChild(newCard);
 	}
 }
 createBoard();
 
 function isTwoCards() {
-	cardsInPlay.push(this.getAttribute('data-card'));
-	if (this.getAttribute('data-card') === 'queen') {
-		this.innerHTML = '<img src="https://pixabay.com/static/uploads/photo/2012/04/11/13/57/clubs-28334_960_720.png" alt="Queen of Clubs" />';  
+    var type = this.getAttribute('data-card');
+	cardsInPlay.push(type);
+	if (type === 'queen') {
+		this.innerHTML = '<img src="queen-clubs.png" alt="Queen of Clubs" />';
 	} else {
-		this.innerHTML = '<img src="https://pixabay.com/static/uploads/photo/2012/04/11/15/10/hearts-28424_960_720.png" alt="King of Hearts" />';
-	}
+		this.innerHTML = '<img src="king-hearts.png" alt="King of Hearts" />';
+	} 
 	if (cardsInPlay.length === 2) {
-		isMatch(cardsInPlay);
+		setTimeout('isMatch(cardsInPlay)',200);
 		cardsInPlay = []; 
 	} 
 }
 
+
 function isMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
-		score = score +1;
-		played = played + 1;
+		score += 1;
+		played += 1;
 		updateStats();
 	} else {
 		alert("Sorry, try again");
-		played = played + 1;
+		played += 1;
 		updateStats();
 	} 
 	for (var i=0; i<cards.length; i++) {
@@ -45,20 +48,18 @@ function isMatch(){
 // stats counters
 var displayedScore = document.getElementById('score');
 var displayedPlayed = document.getElementById('played');
-var displayedSuccess = document.getElementById('success');
 var score= 0;
 var played = 0;
 var resetButton = document.getElementById('reset');
 
 function updateStats() {
   displayedScore.innerHTML=score; // update displayed score
-  displayedPlayed.innerHTML=played; // update displayed score
+  displayedPlayed.innerHTML=played; // update displayed num
 }
 
 function resetStats(){
 	score = 0;
 	played = 0;
-	success = (played / score) * 100;
 	displayedScore.innerHTML=score;
 	displayedPlayed.innerHTML=played;
 }
